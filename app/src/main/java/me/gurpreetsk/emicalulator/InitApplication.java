73a6af5ba38b6e87f75c9061ced721aeb5970258
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-05-05 10:56:27 +0530

    feat: initial commit
    -- setup truecaller sdk, other basic setup

---
+++ app/src/main/java/me/gurpreetsk/emicalulator/InitApplication.java
@@ -0,0 +1,15 @@
+package me.gurpreetsk.emicalulator;
+
+import android.app.Application;
+
+/**
+ * Created by gurpreet on 05/05/17.
+ */
+
+public class InitApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
